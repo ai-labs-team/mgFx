cfb545f78997c21a9559d1e907b5e4aebd149d7d
Warren Seymour <warren@fountainhead.tech>
2020-02-17 12:49:12 +0000

    Clean out old core

--- packages/core/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { singleProcess } from './singleProcess';
-export { IpcMaster } from './Scheduler/IpcMaster';
-export { IpcWorker } from './Executor/IpcWorker';
-export { Context } from './Context';
-export { Task } from './Task';
-export { DispatchStrategy } from './DispatchStrategy';
-export { RoundRobin } from './DispatchStrategy/RoundRobin';
-export { Instrumenter } from './Instrumenter';
