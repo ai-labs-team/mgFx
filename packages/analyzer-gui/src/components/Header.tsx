6059597d091257e30bcbfb2a2eb0fc046d930990
Warren Seymour <warren@fountainhead.tech>
2020-04-29 08:28:16 +0100

    chore: clean-up Header

--- packages/analyzer-gui/src/components/Header.tsx
+++ packages/analyzer-gui/src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Navbar, AnchorButton, Tooltip, Button } from '@blueprintjs/core';
+import { Navbar, Button } from '@blueprintjs/core';
+
+import { useAppContext } from 'src/contexts/App';
 
 import { NavButton } from './NavButton';
-import { useAppContext } from '..//contexts/App';
 
 export const Header: React.FC = () => {
   const { showConfig } = useAppContext();
@@ -10,10 +11,14 @@ export const Header: React.FC = () => {
   return (
     <Navbar>
       <Navbar.Group>
-        <NavButton minimal icon="list" text="Log" to="/" />
-        <Tooltip content="Not Implemented Yet">
-          <AnchorButton minimal disabled icon="gantt-chart" text="Timeline" />
-        </Tooltip>
+        <NavButton exact minimal icon="list" text="Log" to="/log" />
+        <NavButton
+          minimal
+          exact
+          icon="gantt-chart"
+          text="Timeline"
+          to="/timeline"
+        />
       </Navbar.Group>
       <Navbar.Group align="right">
         <Button minimal icon="cog" onClick={showConfig} />
